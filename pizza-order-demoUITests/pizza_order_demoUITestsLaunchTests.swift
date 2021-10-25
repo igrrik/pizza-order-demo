f9cdd60fdfcbde6f319e32240f7b8ff183a028cd
Igor Kokoev <igrrik@gmail.com>
2021-10-26 00:11:25 +0300

    Initial Commit

---
+++ pizza-order-demoUITests/pizza_order_demoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  pizza_order_demoUITestsLaunchTests.swift
+//  pizza-order-demoUITests
+//
+//  Created by Igor Kokoev on 26.10.2021.
+//
+
+import XCTest
+
+class pizza_order_demoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
